3a73b7c990bb61161fe4083b3b72924f1710d514
Igor Wiedler <igor@wiedler.ch>
2012-10-19 18:19:36 +0200

    Add awesome chat example

---
+++ examples/chat.php
@@ -0,0 +1,57 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+function createGifFrame(array $messages)
+{
+    $im = imagecreatetruecolor(500, 400);
+
+    imagefilledrectangle($im, 0, 0, 500, 400, 0x000000);
+    foreach ($messages as $i => $message) {
+        imagestring($im, 3, 40, 20 + $i*20, $message, 0xFFFFFF);
+    }
+
+    ob_start();
+
+    imagegif($im);
+    imagedestroy($im);
+
+    return ob_get_clean();
+}
+
+$loop = React\EventLoop\Factory::create();
+$socket = new React\Socket\Server($loop);
+$http = new React\Http\Server($socket);
+
+$gifServer = new React\GifSocket\Server($loop);
+
+$messages = array();
+$addMessage = function ($message) use ($gifServer, &$messages) {
+    $messages[] = $message;
+    if (count($messages) > 18) {
+        $messages = array_slice($messages, count($messages) - 18);
+    }
+
+    $frame = createGifFrame($messages);
+    $gifServer->addFrame($frame);
+};
+
+$router = new React\GifSocket\Router([
+    '/socket.gif' => $gifServer,
+    '/' => function ($request, $response) {
+        $response->writeHead(200, ['Content-Type' => 'text/html']);
+        $response->end(file_get_contents(__DIR__.'/views/index.html'));
+    },
+    '/message' => function ($request, $response) use ($addMessage) {
+        $message = $request->getQuery()['message'];
+        $addMessage($message);
+
+        $response->writeHead(200);
+        $response->end();
+    },
+]);
+
+$http->on('request', $router);
+
+$socket->listen(8080);
+$loop->run();
