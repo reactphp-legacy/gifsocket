fc6a8602f3929119ea2f26795195f6c9f00cd30c
Igor Wiedler <igor@wiedler.ch>
2012-10-19 18:16:38 +0200

    Bigger image in example, keep track of messages

--- server.php
+++ server.php
@@ -2,12 +2,14 @@
 
 require 'vendor/autoload.php';
 
-function createGifFrame($string)
+function createGifFrame(array $messages)
 {
-    $im = imagecreatetruecolor(200, 80);
+    $im = imagecreatetruecolor(500, 400);
 
-    imagefilledrectangle($im, 0, 0, 99, 99, 0x000000);
-    imagestring($im, 3, 40, 20, $string, 0xFFFFFF);
+    imagefilledrectangle($im, 0, 0, 500, 400, 0x000000);
+    foreach ($messages as $i => $message) {
+        imagestring($im, 3, 40, 20 + $i*20, $message, 0xFFFFFF);
+    }
 
     ob_start();
 
@@ -22,11 +24,24 @@ $socket = new React\Socket\Server($loop);
 $http = new React\Http\Server($socket);
 
 $gifServer = new React\GifSocket\Server($loop);
-$stdin = new React\Stream\Stream(STDIN, $loop);
 
-$stdin->on('data', function ($data) use ($gifServer, $loop) {
-    $frame = createGifFrame(trim($data));
+$messages = array();
+$addMessage = function ($message) use ($gifServer, &$messages) {
+    $messages[] = $message;
+    if (count($messages) > 18) {
+        $messages = array_slice($messages, count($messages) - 18);
+    }
+
+    $frame = createGifFrame($messages);
     $gifServer->addFrame($frame);
+};
+
+$stdin = new React\Stream\Stream(STDIN, $loop);
+$stdin->on('data', function ($data) use ($addMessage) {
+    $messages = explode("\n", trim($data));
+    foreach ($messages as $message) {
+        $addMessage($message);
+    }
 });
 
 $router = new React\GifSocket\Router([
